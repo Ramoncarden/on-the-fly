578d127fcdcd03baff488c4075678797f014c72e
Ramon Cardenas <ramoncarden@gmail.com>
2021-04-26 01:40:38 -0500

    Set up tailwind files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import './tailwind.output.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="border-2 border-gray-300 p-2">Yo</h1>
     </div>
   );
 }
 
 export default App;
+
+// Example pattern for tailwind component
+
+// const classes = {
+//   wrapper: 'border-2 m-16 p-16',
+//   title: 'text-gray-800 text-xl text-center font-bold',
+// };
+
+// const BasicComponent = () => {
+//   return (
+//     <div className={classes.wrapper}>
+//       <h1 className={classes.title}>Basic Component</h1>
+//     </div>
+//   );
+// };
